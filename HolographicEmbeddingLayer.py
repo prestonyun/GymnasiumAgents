523ee6a694fc78d02c56581101b51c3dce9976fa
Preston Yun <prestonyun32@gmail.com>
2023-02-17 20:42:22 -0800

    add defnition for HolographicTransformer

--- HolographicEmbeddingLayer.py
+++ HolographicEmbeddingLayer.py
@@ -1,9 +1,11 @@
-from ast import Tuple
+from typing import Tuple
 from collections import deque
+
 import copy
 import random
 import torch
 import numpy as np
+import torch.nn as nn
 
 class HolographicEmbeddingLayer(torch.nn.Module):
     def __init__(self, vocab_size, embedding_dim):
@@ -14,7 +16,7 @@ class HolographicEmbeddingLayer(torch.nn.Module):
 
     def forward(self, input_indices):
         input_embeddings = self.weights[input_indices]
-        norm = torch.sqrt(torch.sum(input_embeddings ** 2, dim=1, keepdim=True))
+        norm = torch.norm(input_embeddings, dim=1, keepdim=True)
         input_embeddings = input_embeddings / norm
         holo_embedding = torch.sum(input_embeddings, dim=0, keepdim=True)
 
@@ -23,6 +25,9 @@ class HolographicEmbeddingLayer(torch.nn.Module):
 class GameStateDataset(torch.utils.data.Dataset):
     def __init__(self, game_states):
         self.game_states = game_states
+        self.health_range = (0, 121)
+        self.energy_range = (0, 100)
+        self.location_range = (0, 10000)  # assuming location can be negative
 
     def __len__(self):
         return len(self.game_states)
@@ -30,7 +35,25 @@ class GameStateDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         game_state = self.game_states[idx]
 
-        return game_state['health'], game_state['energy'], game_state['location']
+        # normalize and convert health to tensor
+        health = game_state['health']
+        health = np.clip(health, *self.health_range)  # clip to valid range
+        health = (health - self.health_range[0]) / (self.health_range[1] - self.health_range[0])  # normalize
+        health = torch.tensor(health, dtype=torch.float32)
+
+        # normalize and convert energy to tensor
+        energy = game_state['energy']
+        energy = np.clip(energy, *self.energy_range)  # clip to valid range
+        energy = (energy - self.energy_range[0]) / (self.energy_range[1] - self.energy_range[0])  # normalize
+        energy = torch.tensor(energy, dtype=torch.float32)
+
+        # normalize and convert location to tensor
+        location = game_state['location']
+        location = [np.clip(x, *self.location_range) for x in location]  # clip to valid range
+        location = [(x - self.location_range[0]) / (self.location_range[1] - self.location_range[0]) for x in location]  # normalize
+        location = torch.tensor(location, dtype=torch.float32)
+
+        return health, energy, location
 
 class Collator:
     def __init__(self, embedding):
@@ -71,6 +94,51 @@ class ReplayBuffer:
 
     def __len__(self):
         return len(self.buffer)
+
+class DQN(torch.nn.Module):
+    def __init__(self, state_size: int, action_size: int, hidden_size: int):
+        super(DQN, self).__init__()
+        self.fc1 = torch.nn.Linear(state_size, hidden_size)
+        self.fc2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.fc3 = torch.nn.Linear(hidden_size, action_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class HolographicTransformer(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, num_layers, num_heads, hidden_dim, dropout_prob):
+        super(HolographicTransformer, self).__init__()
+
+        # Define the Holographic Embedding layer
+        self.embedding = HolographicEmbeddingLayer(vocab_size, embedding_dim)
+
+        # Define the Transformer Encoder layers
+        encoder_layer = nn.TransformerEncoderLayer(embedding_dim, num_heads, hidden_dim, dropout_prob)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+
+        # Define the output layer
+        self.output_layer = nn.Linear(embedding_dim, 1)
+
+    def forward(self, x):
+        # Apply the Holographic Embedding layer to the input
+        embedded_input = self.embedding(x)
+
+        # Transpose the embedding tensor to prepare it for the Transformer Encoder
+        embedded_input = embedded_input.transpose(0, 1)
+
+        # Apply the Transformer Encoder to the embedded input
+        encoded = self.encoder(embedded_input)
+
+        # Transpose the encoded tensor back to its original shape
+        encoded = encoded.transpose(0, 1)
+
+        # Apply the output layer and return the result
+        output = self.output_layer(encoded)
+        return output.squeeze(-1)
+
 
 
 def run_epoch(model, dataloader, optimizer, device):
