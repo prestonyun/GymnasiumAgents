778f2890eb4a274b932947adf4780aa6794385ec
Preston Yun <prestonyun32@gmail.com>
2023-02-17 23:48:33 -0800

    IT'S WORKING

--- actionset.py
+++ actionset.py
@@ -91,3 +91,29 @@ class Action:
         # perform the action
         pass
 
+from collections import deque
+from typing import Tuple
+import numpy as np
+
+class ReplayBuffer:
+    def __init__(self, buffer_size):
+        self.buffer_size = buffer_size
+        self.buffer = deque(maxlen=buffer_size)
+
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size: int) -> Tuple[np.ndarray]:
+        batch = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, next_states, dones = [], [], [], [], []
+        for i in batch:
+            s, a, r, s2, d = self.buffer[i]
+            states.append(np.array(s, copy=False))
+            actions.append(np.array(a, copy=False))
+            rewards.append(np.array(r, copy=False))
+            next_states.append(np.array(s2, copy=False))
+            dones.append(np.array(d, copy=False))
+        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(dones)
+
+    def __len__(self):
+        return len(self.buffer)
