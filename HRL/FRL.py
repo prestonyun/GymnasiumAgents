9559ce672d53db51723f6a92e90fafb25160a699
Preston Yun <prestonyun32@gmail.com>
2023-03-14 01:59:17 -0700

    begin Bayesian exploration

--- HRL/FRL.py
+++ HRL/FRL.py
@@ -5,8 +5,59 @@ import numpy as np
 import gymnasium as gym
 import torch.nn.functional as F
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class BayesianExplorer:
+    def __init__(self, env, subgoal_shape, agents, replay_buffer_size, batch_size, prior_scale=1.0, kl_threshold=0.5, max_actions=100):
+        self.env = env
+        self.subgoal_shape = subgoal_shape
+        self.agents = agents
+        self.replay_buffer_size = replay_buffer_size
+        self.batch_size = batch_size
+        self.prior_scale = prior_scale
+        self.kl_threshold = kl_threshold
+        self.max_actions = max_actions
+        self.prior = None
+        self.posterior = None
+        self.num_actions = 0
+
+    def update_posterior(self, replay_buffer):
+        if self.prior is None:
+            self.prior = self.agents.get_prior()
+            self.posterior = self.prior.clone()
+        else:
+            self.posterior = self.agents.get_posterior(replay_buffer, self.prior_scale)
+
+    def run(self, num_episodes):
+        obs = self.env.reset()
+        for episode in range(num_episodes):
+            # Reset the posterior distribution every max_actions steps
+            if self.num_actions % self.max_actions == 0:
+                self.update_posterior(self.agents.replay_buffer)
+
+            # Sample an action from the posterior distribution
+            action = self.agents.sample_action(self.posterior, obs)
+
+            # Execute the action and observe the next state and reward
+            next_obs, reward, done, _, _ = self.env.step(action)
+
+            # Update the agent's replay buffer
+            self.agents.update_replay_buffer(obs, action, reward, next_obs, done)
+
+            # Update the posterior distribution if we've collected enough data
+            if len(self.agents.replay_buffer) >= self.agents.batch_size:
+                kl_divergence = self.agents.compute_kl_divergence(self.posterior, self.agents.get_prior())
+                if kl_divergence > self.kl_threshold:
+                    self.update_posterior(self.agents.replay_buffer)
+
+            # Prepare for the next step
+            obs = next_obs
+            self.num_actions += 1
+
+            if done:
+                obs = self.env.reset()
+                print("Episode {} - Total reward: {}".format(episode, self.agent.total_reward))
+                self.agents.total_reward = 0
 
 # Define the high-level manager
 class HighLevelManager(nn.Module):
@@ -42,10 +93,9 @@ class LowLevelWorker(nn.Module):
         action = torch.clamp(action_mean + action_noise, 0.0, 1.0) * 2.0 - 1.0
         return action
 
-
 # Define the SAC algorithm
 class SAC:
-    def __init__(self, env, subgoal_shape, worker_models, worker_lr=1e-4, critic_lr=1e-3, actor_lr=1e-3, gamma=0.99, alpha=0.2, alpha_lr=1e-4, min_alpha=0.01, tau=0.005, replay_buffer_size=int(1e6), batch_size=128):
+    def __init__(self, env, subgoal_shape, worker_models, worker_lr=1e-4, critic_lr=1e-3, actor_lr=1e-3, gamma=0.99, alpha=0.2, alpha_lr=1e-4, min_alpha=0.01, tau=0.005, replay_buffer_size=int(1e6), batch_size=128, prior_std=0.1):
         self.env = env
         self.subgoal_shape = subgoal_shape
         self.worker_models = worker_models
@@ -69,6 +119,10 @@ class SAC:
         self.high_level_actor_optimizer = optim.Adam(self.high_level_actor.parameters(), lr=actor_lr)
         self.high_level_critic_optimizer = optim.Adam(list(self.high_level_critic1.parameters()) + list(self.high_level_critic2.parameters()), lr=critic_lr)
         self.high_level_entropy_optimizer = optim.Adam(self.high_level_actor.parameters(), lr=actor_lr)
+
+        self.prior_mean = np.zeros(subgoal_shape)
+        self.prior_std = prior_std
+        # self.explorer = BayesianExplorer(env, self.subgoal_shape, self.worker_models, self.replay_buffer_size, self.batch_size)
 
         # Define the critic and entropy optimizer for the low-level workers
         self.worker_critic1s = []
@@ -94,6 +148,32 @@ class SAC:
         # Initialize the replay buffer
         self.replay_buffer = []
         self.replay_buffer_index = 0
+
+    def get_prior(self):
+        return self.explorer.get_prior()
+    
+    def get_posterior(self, prior):
+        # Compute the posterior distribution for the current state
+        return self.explorer.get_posterior()
+    
+    def compute_kl_divergence(self):
+        return self.explorer.compute_kl_divergence()
+
+    
+    def sample_action(self, obs, subgoal):
+        with torch.no_grad():
+           subgoal_tensor = torch.FloatTensor(subgoal).unsqueeze(0).to(device)
+           state_tensor = torch.FloatTensor(obs).unsqueeze(0).to(device)
+           action = np.zeros((len(self.worker_models), 1))
+           for i in range(len(self.worker_models)):
+               action[i] = self.worker_models[i](torch.cat((state_tensor, subgoal_tensor), dim=1)).cpu().numpy()
+        return action.squeeze()
+
+    def update_replay_buffer(self, state, subgoal, action, reward, next_state, done):
+        self.replay_buffer.append((state, subgoal, action, reward, next_state, done))
+        self.replay_buffer_index = (self.replay_buffer_index + 1) % self.replay_buffer_size
+        while len(self.replay_buffer) > self.replay_buffer_size:
+            self.replay_buffer.pop(0)            
 
     # Compute the Q-value for the high-level manager
     def compute_high_level_q(self, obs, subgoal):
@@ -190,27 +270,25 @@ class SAC:
             critic_loss.backward()
             self.worker_critic_optimizer.step()
 
-
-    def update_alpha(self, step):
-        self.alpha = max(self.alpha * (1-step / 100000), 0.01)
-
     # Train the SAC algorithm
     def train(self, num_episodes):
         obs, _ = self.env.reset()
         for episode in range(num_episodes):
-            #env.render()
+            # env.render()
             subgoal = self.high_level_actor(torch.FloatTensor(obs).to(device)).detach().cpu().numpy()
             done = False
             total_reward = 0
             episode_steps = 0
             action = np.zeros((len(self.worker_models), 1))
-            while not done and episode_steps < self.env._max_episode_steps:
+            while not done and episode_steps < 1500:
                 # Choose an action from the low-level worker
                 for i in range(len(self.worker_models)):
                     action[i] = self.worker_models[i](torch.FloatTensor(obs).to(device)).detach().cpu().numpy()
                 action = action.squeeze()
+
                 # Execute the action and observe the next state and reward
                 next_obs, reward, done, _, _ = self.env.step(action)
+
                 # Store the transition in the replay buffer
                 self.replay_buffer.append((obs, subgoal, action, reward, next_obs, done))
                 while len(self.replay_buffer) > self.replay_buffer_size:
@@ -218,6 +296,12 @@ class SAC:
 
                 # Update the high-level manager and low-level workers
                 if len(self.replay_buffer) > self.batch_size:
+                    # Update the Bayesian explorer
+                    # self.explorer.update_posterior(self.replay_buffer)
+                    # Choose the subgoal using Bayesian exploration
+                    # subgoal = self.explorer.choose_subgoal(obs)
+                    # self.high_level_actor.temperature = self.explorer.get_temperature()
+                    # Update the high-level manager and low-level workers using SAC
                     self.update_high_level()
                     self.update_workers()
 
@@ -230,6 +314,10 @@ class SAC:
             #print(subgoal)
             obs, _ = self.env.reset()
 
+        torch.save(self.high_level_actor.state_dict(), 'high_level_actor.pth')
+        for i, model in enumerate(self.worker_models):
+            torch.save(model.state_dict(), 'worker_model_{}.pth'.format(i)) 
+
 # Test the SAC algorithm
 env = gym.make('BipedalWalker-v3')#, render_mode='human')
 subgoal_shape = (4,)
@@ -237,7 +325,4 @@ worker_models = [LowLevelWorker(env.observation_space.shape[0], env.action_space
 
 sac = SAC(env, subgoal_shape, worker_models)
 
-sac.train(100)
-torch.save(sac.high_level_actor.state_dict(), 'high_level_actor.pth')
-for i, model in enumerate(sac.worker_models):
-    torch.save(model.state_dict(), 'worker_model_{}.pth'.format(i))
+sac.train(1000)
